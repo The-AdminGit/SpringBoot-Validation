b56e3c57fe080a1a4124f233b25c98c0e5473458
vivek kumar sah <viveksah4ui@gmail.com>
2024-04-04 20:38:31 +0530

    validation

---
+++ src/main/java/com/validation/learningspringbootvalidation/LearningSpringbootValidationApplication.java
@@ -0,0 +1,13 @@
+package com.validation.learningspringbootvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringbootValidationApplication.class, args);
+	}
+
+}
